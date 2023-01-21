4015249663f68d3089cae94c2e49830afa44df11
Muhammad Imran Manzoor <aliemranjazib@gmail.com>
2023-01-21 21:18:23 +0500

    location in chat

--- lib/chat_module/message_text_field.dart
+++ lib/chat_module/message_text_field.dart
@@ -1,6 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
 
 class MessageTextField extends StatefulWidget {
   final String currentId;
@@ -15,6 +18,78 @@ class MessageTextField extends StatefulWidget {
 
 class _MessageTextFieldState extends State<MessageTextField> {
   TextEditingController _controller = TextEditingController();
+  Position? _curentPosition;
+  String? _curentAddress;
+  String? message;
+  LocationPermission? permission;
+  Future _getCurrentLocation() async {
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      Fluttertoast.showToast(msg: "Location permissions are  denind");
+      if (permission == LocationPermission.deniedForever) {
+        Fluttertoast.showToast(
+            msg: "Location permissions are permanently denind");
+      }
+    }
+    Geolocator.getCurrentPosition(
+            desiredAccuracy: LocationAccuracy.high,
+            forceAndroidLocationManager: true)
+        .then((Position position) {
+      setState(() {
+        _curentPosition = position;
+        print(_curentPosition!.latitude);
+        _getAddressFromLatLon();
+      });
+    }).catchError((e) {
+      Fluttertoast.showToast(msg: e.toString());
+    });
+  }
+
+  _getAddressFromLatLon() async {
+    try {
+      List<Placemark> placemarks = await placemarkFromCoordinates(
+          _curentPosition!.latitude, _curentPosition!.longitude);
+
+      Placemark place = placemarks[0];
+      setState(() {
+        _curentAddress =
+            "${place.locality},${place.postalCode},${place.street},";
+      });
+    } catch (e) {
+      Fluttertoast.showToast(msg: e.toString());
+    }
+  }
+
+  sendMessage(String message, String type) async {
+    await FirebaseFirestore.instance
+        .collection('users')
+        .doc(widget.currentId)
+        .collection('messages')
+        .doc(widget.friendId)
+        .collection('chats')
+        .add({
+      'senderId': widget.currentId,
+      'receiverId': widget.friendId,
+      'message': message,
+      'type': type,
+      'date': DateTime.now(),
+    });
+    await FirebaseFirestore.instance
+        .collection('users')
+        .doc(widget.friendId)
+        .collection('messages')
+        .doc(widget.currentId)
+        .collection('chats')
+        .add({
+      'senderId': widget.currentId,
+      'receiverId': widget.friendId,
+      'message': message,
+      'type': type,
+      'date': DateTime.now(),
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -53,33 +128,9 @@ class _MessageTextFieldState extends State<MessageTextField> {
               padding: const EdgeInsets.all(8.0),
               child: InkWell(
                 onTap: () async {
+                  message = _controller.text;
+                  sendMessage(message!, 'text');
                   _controller.clear();
-                  await FirebaseFirestore.instance
-                      .collection('users')
-                      .doc(widget.currentId)
-                      .collection('messages')
-                      .doc(widget.friendId)
-                      .collection('chats')
-                      .add({
-                    'senderId': widget.currentId,
-                    'receiverId': widget.friendId,
-                    'message': _controller.text,
-                    'type': 'text',
-                    'date': DateTime.now(),
-                  });
-                  await FirebaseFirestore.instance
-                      .collection('users')
-                      .doc(widget.friendId)
-                      .collection('messages')
-                      .doc(widget.currentId)
-                      .collection('chats')
-                      .add({
-                    'senderId': widget.currentId,
-                    'receiverId': widget.friendId,
-                    'message': _controller.text,
-                    'type': 'text',
-                    'date': DateTime.now(),
-                  });
                 },
                 child: Icon(
                   Icons.send,
@@ -104,7 +155,14 @@ class _MessageTextFieldState extends State<MessageTextField> {
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            chatsIcon(Icons.location_pin, "location", () {}),
+            chatsIcon(Icons.location_pin, "location", () async {
+              await _getCurrentLocation();
+              Future.delayed(Duration(seconds: 2), () {
+                message =
+                    "https://www.google.com/maps/search/?api=1&query=${_curentPosition!.latitude}%2C${_curentPosition!.longitude}. $_curentAddress";
+                sendMessage(message!, "link");
+              });
+            }),
             chatsIcon(Icons.camera_alt, "Camera", () {}),
             chatsIcon(Icons.insert_photo, "Photo", () {}),
           ],
--- lib/chat_module/singleMessage.dart
+++ lib/chat_module/singleMessage.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class SingleMessage extends StatelessWidget {
   final String? message;
@@ -25,56 +26,116 @@ class SingleMessage extends StatelessWidget {
     final size = MediaQuery.of(context).size;
     DateTime d = DateTime.parse(date!.toDate().toString());
     String cdate = "${d.hour}" + ":" + "${d.minute}";
-    return Container(
-      constraints: BoxConstraints(
-        maxWidth: size.width / 2,
-      ),
-      alignment: isMe! ? Alignment.centerRight : Alignment.centerLeft,
-      padding: EdgeInsets.all(10),
-      child: Container(
-          decoration: BoxDecoration(
-            color: isMe! ? Colors.pink : Colors.black,
-            borderRadius: isMe!
-                ? BorderRadius.only(
-                    topLeft: Radius.circular(15),
-                    topRight: Radius.circular(15),
-                    bottomLeft: Radius.circular(15),
-                  )
-                : BorderRadius.only(
-                    topLeft: Radius.circular(15),
-                    topRight: Radius.circular(15),
-                    bottomRight: Radius.circular(15),
-                  ),
-          ),
-          padding: EdgeInsets.all(10),
-          constraints: BoxConstraints(
-            maxWidth: size.width / 2,
-          ),
-          alignment: isMe! ? Alignment.centerRight : Alignment.centerLeft,
-          child: Column(
-            children: [
-              Align(
-                  alignment: Alignment.centerRight,
-                  child: Text(
-                    isMe! ? myName! : friendName!,
-                    style: TextStyle(fontSize: 15, color: Colors.white70),
-                  )),
-              Divider(),
-              Align(
-                  alignment: Alignment.centerRight,
-                  child: Text(
-                    message!,
-                    style: TextStyle(fontSize: 18, color: Colors.white),
-                  )),
-              Divider(),
-              Align(
-                  alignment: Alignment.centerRight,
-                  child: Text(
-                    "$cdate",
-                    style: TextStyle(fontSize: 15, color: Colors.white70),
-                  )),
-            ],
-          )),
-    );
+    return type == "text"
+        ? Container(
+            constraints: BoxConstraints(
+              maxWidth: size.width / 2,
+            ),
+            alignment: isMe! ? Alignment.centerRight : Alignment.centerLeft,
+            padding: EdgeInsets.all(10),
+            child: Container(
+                decoration: BoxDecoration(
+                  color: isMe! ? Colors.pink : Colors.black,
+                  borderRadius: isMe!
+                      ? BorderRadius.only(
+                          topLeft: Radius.circular(15),
+                          topRight: Radius.circular(15),
+                          bottomLeft: Radius.circular(15),
+                        )
+                      : BorderRadius.only(
+                          topLeft: Radius.circular(15),
+                          topRight: Radius.circular(15),
+                          bottomRight: Radius.circular(15),
+                        ),
+                ),
+                padding: EdgeInsets.all(10),
+                constraints: BoxConstraints(
+                  maxWidth: size.width / 2,
+                ),
+                alignment: isMe! ? Alignment.centerRight : Alignment.centerLeft,
+                child: Column(
+                  children: [
+                    Align(
+                        alignment: Alignment.centerRight,
+                        child: Text(
+                          isMe! ? myName! : friendName!,
+                          style: TextStyle(fontSize: 15, color: Colors.white70),
+                        )),
+                    Divider(),
+                    Align(
+                        alignment: Alignment.centerRight,
+                        child: Text(
+                          message!,
+                          style: TextStyle(fontSize: 18, color: Colors.white),
+                        )),
+                    Divider(),
+                    Align(
+                        alignment: Alignment.centerRight,
+                        child: Text(
+                          "$cdate",
+                          style: TextStyle(fontSize: 15, color: Colors.white70),
+                        )),
+                  ],
+                )),
+          )
+        : Container(
+            constraints: BoxConstraints(
+              maxWidth: size.width / 2,
+            ),
+            alignment: isMe! ? Alignment.centerRight : Alignment.centerLeft,
+            padding: EdgeInsets.all(10),
+            child: Container(
+                decoration: BoxDecoration(
+                  color: isMe! ? Colors.pink : Colors.black,
+                  borderRadius: isMe!
+                      ? BorderRadius.only(
+                          topLeft: Radius.circular(15),
+                          topRight: Radius.circular(15),
+                          bottomLeft: Radius.circular(15),
+                        )
+                      : BorderRadius.only(
+                          topLeft: Radius.circular(15),
+                          topRight: Radius.circular(15),
+                          bottomRight: Radius.circular(15),
+                        ),
+                ),
+                padding: EdgeInsets.all(10),
+                constraints: BoxConstraints(
+                  maxWidth: size.width / 2,
+                ),
+                alignment: isMe! ? Alignment.centerRight : Alignment.centerLeft,
+                child: Column(
+                  children: [
+                    Align(
+                        alignment: Alignment.centerRight,
+                        child: Text(
+                          isMe! ? myName! : friendName!,
+                          style: TextStyle(fontSize: 15, color: Colors.white70),
+                        )),
+                    Divider(),
+                    Align(
+                        alignment: Alignment.centerRight,
+                        child: GestureDetector(
+                          onTap: () async {
+                            await launchUrl(Uri.parse("$message"));
+                          },
+                          child: Text(
+                            message!,
+                            style: TextStyle(
+                                fontStyle: FontStyle.italic,
+                                fontSize: 16,
+                                color: Colors.white),
+                          ),
+                        )),
+                    Divider(),
+                    Align(
+                        alignment: Alignment.centerRight,
+                        child: Text(
+                          "$cdate",
+                          style: TextStyle(fontSize: 15, color: Colors.white70),
+                        )),
+                  ],
+                )),
+          );
   }
 }
