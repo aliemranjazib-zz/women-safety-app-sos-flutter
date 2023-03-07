afb656d7a1980cb20aff94ef59ba79b2f7395539
Muhammad Imran Manzoor <aliemranjazib@gmail.com>
2023-03-07 20:20:53 +0500

    shake

--- lib/child/bottom_screens/child_home_page.dart
+++ lib/child/bottom_screens/child_home_page.dart
@@ -1,6 +1,14 @@
 import 'dart:math';
 
+import 'package:background_sms/background_sms.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:shake/shake.dart';
+import 'package:women_safety_app/db/db_services.dart';
+import 'package:women_safety_app/model/contactsm.dart';
 import 'package:women_safety_app/widgets/home_widgets/CustomCarouel.dart';
 import 'package:women_safety_app/widgets/home_widgets/custom_appBar.dart';
 import 'package:women_safety_app/widgets/home_widgets/emergency.dart';
@@ -15,6 +23,61 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   // const HomeScreen({super.key});
   int qIndex = 0;
+  Position? _curentPosition;
+  String? _curentAddress;
+  LocationPermission? permission;
+  _getPermission() async => await [Permission.sms].request();
+  _isPermissionGranted() async => await Permission.sms.status.isGranted;
+  _sendSms(String phoneNumber, String message, {int? simSlot}) async {
+    SmsStatus result = await BackgroundSms.sendMessage(
+        phoneNumber: phoneNumber, message: message, simSlot: 1);
+    if (result == SmsStatus.sent) {
+      print("Sent");
+      Fluttertoast.showToast(msg: "send");
+    } else {
+      Fluttertoast.showToast(msg: "failed");
+    }
+  }
+
+  _getCurrentLocation() async {
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      Fluttertoast.showToast(msg: "Location permissions are  denind");
+      if (permission == LocationPermission.deniedForever) {
+        await Geolocator.requestPermission();
+        Fluttertoast.showToast(
+            msg: "Location permissions are permanently denind");
+      }
+    }
+    Geolocator.getCurrentPosition(
+            desiredAccuracy: LocationAccuracy.high,
+            forceAndroidLocationManager: true)
+        .then((Position position) {
+      setState(() {
+        _curentPosition = position;
+        print(_curentPosition!.latitude);
+        _getAddressFromLatLon();
+      });
+    }).catchError((e) {
+      Fluttertoast.showToast(msg: e.toString());
+    });
+  }
+
+  _getAddressFromLatLon() async {
+    try {
+      List<Placemark> placemarks = await placemarkFromCoordinates(
+          _curentPosition!.latitude, _curentPosition!.longitude);
+
+      Placemark place = placemarks[0];
+      setState(() {
+        _curentAddress =
+            "${place.locality},${place.postalCode},${place.street},";
+      });
+    } catch (e) {
+      Fluttertoast.showToast(msg: e.toString());
+    }
+  }
 
   getRandomQuote() {
     Random random = Random();
@@ -23,10 +86,45 @@ class _HomeScreenState extends State<HomeScreen> {
     });
   }
 
+  getAndSendSms() async {
+    List<TContact> contactList = await DatabaseHelper().getContactList();
+
+    String messageBody =
+        "https://maps.google.com/?daddr=${_curentPosition!.latitude},${_curentPosition!.longitude}";
+    if (await _isPermissionGranted()) {
+      contactList.forEach((element) {
+        _sendSms("${element.number}", "i am in trouble $messageBody");
+      });
+    } else {
+      Fluttertoast.showToast(msg: "something wrong");
+    }
+  }
+
   @override
   void initState() {
     getRandomQuote();
     super.initState();
+    _getPermission();
+    _getCurrentLocation();
+    ////// shake feature ///
+    ShakeDetector.autoStart(
+      onPhoneShake: () {
+        getAndSendSms();
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text('Shake!'),
+          ),
+        );
+        // Do stuff on phone shake
+      },
+      minimumShakeCount: 1,
+      shakeSlopTimeMS: 500,
+      shakeCountResetTime: 3000,
+      shakeThresholdGravity: 2.7,
+    );
+
+    // To close: detector.stopListening();
+    // ShakeDetector.waitForStart() waits for user to call detector.startListening();
   }
 
   @override
